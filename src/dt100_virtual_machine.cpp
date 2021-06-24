db85f5b6f07161bf5a3309903c67217752dc52d0
adthoms <adthoms@uwaterloo.ca>
2021-06-24 17:36:02 -0400

    auto format using Clang

--- src/dt100_virtual_machine.cpp
+++ src/dt100_virtual_machine.cpp
@@ -2,22 +2,20 @@
 #include <stdlib.h>
 #include <signal.h>
 
-void shutdown(int sig)
-{
-    system("VBoxManage controlvm \"XP_32\" acpipowerbutton");
-    ros::shutdown();
+void shutdown(int sig) {
+  system("VBoxManage controlvm \"XP_32\" acpipowerbutton");
+  ros::shutdown();
 }
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "dt100_virtual_machine");
-    ros::NodeHandle nh;
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "dt100_virtual_machine");
+  ros::NodeHandle nh;
 
-    ROS_INFO("dt100_virtual_machine initialize");
+  ROS_INFO("dt100_virtual_machine initialize");
 
-    system("VBoxManage startvm \"XP_32\"");
-    signal(SIGINT, shutdown);
+  system("VBoxManage startvm \"XP_32\"");
+  signal(SIGINT, shutdown);
 
-    ros::spin();
-    return 0;
+  ros::spin();
+  return 0;
 }
