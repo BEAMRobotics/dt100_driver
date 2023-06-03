bc119f1d81886f3db095a8e9ab83d81f5a5727fc
adthoms <adthoms@uwaterloo.ca>
2023-06-03 16:08:42 -0700

    clean up vm node

--- scripts/dt100_vm.bash
+++ scripts/dt100_vm.bash
@@ -3,23 +3,25 @@ rosbash_init_node "dt100_vm_node" "$@"  # parse the command line arguments
 
 # get params
 rosbash_param bridge_adapter_ "bridge_adapter"
-rosbash_param add_network_interface_ "add_network_interface" "True"
 rosbash_param headless_ "headless" "True"
 
-# just print this out
-echo "Launching DT100 Virtual Machine..."
+# print params
+echo "dt100_vm_node parameters loaded:"
+echo "bridge_adapter_: ${bridge_adapter_}"
+echo "headless_: ${bridge_adapter_}"
 
-# set bridged adapter 
+# set bridged adapter
 VBoxManage modifyvm "Windows_XP_32_DT100" --nic1 none
 VBoxManage modifyvm "Windows_XP_32_DT100" --nic1 bridged --bridgeadapter1 $bridge_adapter_
 
 # start vm
+echo "Launching DT100 Virtual Machine..."
 if $headless_; then
   VBoxManage startvm "Windows_XP_32_DT100" --type headless
 else
   VBoxManage startvm "Windows_XP_32_DT100"
 fi
-echo "here3"
+
 # exit gracefully by returning a status 
 trap ctrlc SIGINT
 
